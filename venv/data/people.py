c30e40698b4c29732e8d953f41b2297bcb11596b
yuyao-cyber <64072369+yuyao-cyber@users.noreply.github.com>
2020-11-28 14:08:03 -0800

    Update people.py

--- venv/data/people.py
+++ venv/data/people.py
@@ -1,9 +1,6 @@
 import mongoengine as db
 import datetime
 import Ticket
-
-
-import json
 import os
 
 
@@ -15,14 +12,23 @@ class People(db.Document):
     username = db.StringField(required = True, unique = True)
     password = db.StringField(required = True)
     email = db.StringField(required = True, unique = True)
-    team = db.ReferenceField('team.Team', DBref = False)
-
+    team = db.ListField(db.ReferenceField('team.Team', DBref = False))
+    project = db.ListField(db.ReferenceField('Project.Project'))
     ticket_list = db.ListField(db.ReferenceField('ticket.Ticket', DBref = False),  required = False)
+
+    def assign_project_to_people(self, project):
+        self.project.append(project)
+
+    def display_people(self):
+        print("username: {}".format(self.username))
+        print("first_name: {}".format(self.first_name))
+        print("last_name: {}".format(self.last_name))
 
     meta = {
         'abstract':True,
         'allow_inheritance': True,
     }
+
 
     # def get_name(self):
     #     pass
@@ -62,16 +68,25 @@ class Developer(People):
 # employee1 = People(first_name = "yuyao",last_name = "zhuge",username = "yzhuge",password = "9801",email = "gmail.com")
 # print(employee1.email)
 
-# class Admin(People):
-#
-#     def submit_ticket(self, ticket):
-#         # make it submit all tickets
-#         # make it only submit feature ticket
-#         pass
-#
-#     meta = {
-#         'db_alias': 'core',
-#         'collection':'People',
-#         'indexes':["username", "email"],
-#         'ordering': ["-register_date"]
-#     }
+class Admin(People):
+
+    def people_search(self, first_name=None, last_name=None):
+        if first_name is None and last_name is None:
+            print("No parameter entered.")
+            return None
+        elif first_name is None:
+            return People.last_name.search_text(last_name).order_by('$text_score')
+        else:
+            return People.first_name.search_text(first_name).order_by('$text_score')
+
+    def submit_ticket(self, ticket):
+        # make it submit all tickets
+        # make it only submit feature ticket
+        pass
+
+    meta = {
+        'db_alias': 'core',
+        'collection':'People',
+        'indexes':["username", "email"],
+        'ordering': ["-register_date"]
+    }
