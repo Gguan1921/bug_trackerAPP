e79f8164c4e22a074f1e63d1b59022ad54624b52
yuyao-cyber <64072369+yuyao-cyber@users.noreply.github.com>
2020-11-28 11:53:15 -0800

    Update team.py

--- venv/data/team.py
+++ venv/data/team.py
@@ -1,19 +1,37 @@
-from people.py import People
 
+import people
+import mongoengine as db
+import datetime
+import json
 
-class Team:
-    pass
-    meta = {
-        'db_alias': 'core',
-        'collection': 'teams'
-    }
+import Project
 
-    def add_people_to_team(cls, name):
-        #have to find the people object in DB and add into the team object
+class Team (db.Document):
+  name = db.StringField(required = True)
+  birthday = db.DateTimeField(default = datetime.datetime.now)
+  member = db.ListField(db.ReferenceField('people.People'))
+  project = db.ListField(db.ReferenceField('Project.Project'))
 
+  def assign_project_to_team(self, project):
+      self.project.append(project)
 
-    def transfer_people_to_team(target, source, name):
-        #tranfer a people from source team to target team
+  def add_people_to_team(self, people):
+      #have to find the people object in DB and add into the team object
+      if people not in self.member:
+          self.member.append(people)
+      self.save()
 
+  def transfer_people_to_team(self, target, people):
+      #tranfer a person from source team to target team
+      if people in self.member:
+          self.member.remove(people)
+      # else:
+      #     raise Exception("{} is not found in the team: {}".format(people.first_name + ' ' + people.last_name, self.name))
+          target.member.append(people)
 
-
+  meta = {
+    'allow_inheritance': True,
+    'db_alias': 'core',
+    'collection':'Teams',
+    'indexes':["name"]
+  }
