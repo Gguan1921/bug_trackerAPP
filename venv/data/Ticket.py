e0e14c27b9a4b166dc56965f593b0bbb736ceda7
yuyao-cyber <64072369+yuyao-cyber@users.noreply.github.com>
2020-11-28 11:41:47 -0800

    Update Ticket.py

--- venv/data/Ticket.py
+++ venv/data/Ticket.py
@@ -1,27 +1,26 @@
 import mongoengine as db
-import data.people as people
+import people
 import datetime
 
 class Ticket (db.Document):
-    birthday = db.DateTimeField(default = datetime.datetime.now)
-    title = db.StringField(required = True)
-    comment = db.StringField(required = True)
-    priority = db.IntField(required = True, max_value = 10, min_value = 1)
-    creater = db.ReferenceField('people.People', required = True, DBref = False)
-    compeleted = db.BooleanField(default = False)
+  birthday = db.DateTimeField(default = datetime.datetime.now)
+  assigned_to_person = db.ReferenceField('people.People', required = True, DBref = False)
+  title = db.StringField(required = True)
+  comment = db.ListField(db.StringField(required = True))
+  priority = db.IntField(required = True, max_value = 10, min_value = 1)
+  creater = db.ReferenceField('people.People', required = True, DBref = False)
+  compeleted = db.BooleanField(default = False)
 
-    #project = db.RefrencField ('Project.Project', required = True, Dbref = False)
-
-    meta = {
-        'allow_inheritance': True,
-        'db_alias': 'core',
-        'collection':'Ticket',
-        'indexes':["title", "id"],
-        'ordering': ["-birthday"]
-    }
+  meta = {
+      'allow_inheritance': True,
+      'db_alias': 'core',
+      'collection':'Ticket',
+      'indexes':["title", "id"],
+      'ordering': ["-birthday"]
+  }
 
 class Feature_ticket (Ticket):
-    pass
+  pass
 
 class Bug_ticket (Ticket):
-    pass
+  pass
